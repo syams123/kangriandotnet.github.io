88393d803ad8ab0c4ebd8bc10bdab023e9347c46
kangriandotnet <72417953+kangriandotnet@users.noreply.github.com>
2022-05-27 21:41:57 +0700

    global update

--- template/toko-whatsapp-wordpress-theme.js
+++ template/toko-whatsapp-wordpress-theme.js
@@ -1,3 +1,24 @@
+/* ================================================================================================
+
+    Peringatan !
+
+===================================================================================================
+
+    Mengubah / Menghapus file javascript (js) ini dapat mengakibatkan :
+
+    1. Kerusakan / error pada template
+    2. Menghilangkan fungsi update / perbaikan otomatis pada template
+    3. Tidak akan mendapatkan support / bantuan dari kreator template
+
+    Terima kasih sudah menggunakan karya saya. :)
+
+    Salam,
+    KangRian.NET
+
+    Terakhir Diperbarui : 27/5/2022 @ 21:40
+
+================================================================================================ */
+    
 (function($) {
     
     $('.titleCase').each(function() {
